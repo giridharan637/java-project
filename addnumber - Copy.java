2ae85acd5e60fb149f270c717184c8aeada2e5a8
giridharan637 <girivasantha2007@gmail.com>
2025-03-07 21:02:57 +0530

    numbers

---
+++ addnumber - Copy.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class addnumber
+{
+    public static void main(String[] args)
+     {
+        Scanner a = new Scanner(System.in);
+       int b = a.nextInt();
+        int c = a.nextInt();
+         System.out.println(b);
+        System.out.println( c);
+       int d = b+c;
+       System.out.println(d);
+    }
+}
